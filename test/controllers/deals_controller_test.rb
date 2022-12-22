eb996380630a60914a381fa739f05194b7d2054b
entcher <49116032+entcher@users.noreply.github.com>
2022-12-22 14:00:38 +0300

    edit deals_controller_tests

--- test/controllers/deals_controller_test.rb
+++ test/controllers/deals_controller_test.rb
@@ -4,45 +4,5 @@ class DealsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @deal = deals(:one)
   end
-
-  test "should get index" do
-    get deals_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_deal_url
-    assert_response :success
-  end
-
-  test "should create deal" do
-    assert_difference("Deal.count") do
-      post deals_url, params: { deal: { amount: @deal.amount, currency_id: @deal.currency_id, portfolio_id: @deal.portfolio_id, price: @deal.price } }
-    end
-
-    assert_redirected_to deal_url(Deal.last)
-  end
-
-  test "should show deal" do
-    get deal_url(@deal)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_deal_url(@deal)
-    assert_response :success
-  end
-
-  test "should update deal" do
-    patch deal_url(@deal), params: { deal: { amount: @deal.amount, currency_id: @deal.currency_id, portfolio_id: @deal.portfolio_id, price: @deal.price } }
-    assert_redirected_to deal_url(@deal)
-  end
-
-  test "should destroy deal" do
-    assert_difference("Deal.count", -1) do
-      delete deal_url(@deal)
-    end
-
-    assert_redirected_to deals_url
-  end
+  
 end
