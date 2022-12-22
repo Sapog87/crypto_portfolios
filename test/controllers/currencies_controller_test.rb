d5d961c3cf1b6f87970cd444e2606d18502090a8
entcher <49116032+entcher@users.noreply.github.com>
2022-12-22 14:02:18 +0300

    edit currencies_controller tests

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -10,39 +10,8 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_currency_url
-    assert_response :success
-  end
-
-  test "should create currency" do
-    assert_difference("Currency.count") do
-      post currencies_url, params: { currency: { symbol: @currency.symbol } }
-    end
-
-    assert_redirected_to currency_url(Currency.last)
-  end
-
   test "should show currency" do
     get currency_url(@currency)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_currency_url(@currency)
-    assert_response :success
-  end
-
-  test "should update currency" do
-    patch currency_url(@currency), params: { currency: { symbol: @currency.symbol } }
-    assert_redirected_to currency_url(@currency)
-  end
-
-  test "should destroy currency" do
-    assert_difference("Currency.count", -1) do
-      delete currency_url(@currency)
-    end
-
-    assert_redirected_to currencies_url
   end
 end
